affbf83ab610532f2ca9807c87cca08a84dfb925
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-18 13:25:23 +0100

    Expect #top_up to add amount to #balance

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -4,11 +4,16 @@ describe OysterCard do
   subject { OysterCard.new }
   alias_method :oyster_card, :subject
 
-  it { is_expected.to respond_to :balance }
-
   describe '#initialize' do
     it 'initializes with a balance of 0' do
       expect(oyster_card.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'increases the balance by the specified amount' do
+      oyster_card.top_up(5)
+      expect(oyster_card.balance).to eq 5
     end
   end
 
